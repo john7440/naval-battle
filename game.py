5bddf0108242fdd2311da811e98aaa70a6722040
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-20 13:41:28 +0200

    ajout de l'init et de la liste des bateaux créés avec la classe Boats

--- game.py
+++ game.py
@@ -1,3 +1,20 @@
+from grid import Grid
+from boats import Boats
+
+
 class Game:
- def __init__(self):
-     ...
+
+    def __init__(self):
+        self.grid = Grid()
+        self.boats =self.setup_of_boats()
+        self.played_coords = set()
+
+    @staticmethod
+    def setup_of_boats():
+         return [
+             Boats('aircraft', 5, [('B', '2'), ('C', '2'), ('D', '2'), ('E', '2'), ('F', '2')]),
+             Boats('cruiser', 4, [('A', '4'), ('A', '5'), ('A', '6'), ('A', '7')]),
+             Boats('destroyer', 3, [('C', '5'), ('C', '6'), ('C', '7')]),
+             Boats('submarine', 3, [('H', '5'), ('I', '5'), ('J', '5')]),
+             Boats('torpedo', 2, [('E', '9'), ('F', '9')])
+         ]
