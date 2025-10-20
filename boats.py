5ae5fdcd3cd92aa2f0c4c1f7ec29c5357abcf720
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-20 12:56:33 +0200

    ajout d'une fonction pour comptabiliser si un bateau est touché et si il est coulé dans notre classe Boats

--- boats.py
+++ boats.py
@@ -1,8 +1,19 @@
 
 
 class Boats:
-    def __init__(self, name, part, pos, touche):
+
+
+    def __init__(self, name, part, pos):
         self.name = name
         self.part = part
-        self.pos = pos
-        self.touche = touche
+        self.pos = set(pos)
+        self.hits = set()
+
+    def is_hit(self, coord):
+        if coord in self.pos:
+            self.hits.add(coord)
+            return True
+        return False
+
+    def is_sunk(self):
+        return self.pos == self.hits
