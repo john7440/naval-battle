9620c05ca9ddf95edcbb6039cc29126f0bb4d877
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-21 14:01:36 +0200

    modification de setup boats pour refléter l'abandon du paramètre part

--- game.py
+++ game.py
@@ -21,11 +21,11 @@ class Game:
         :return: a list of boats.
         """
         return [
-             Boats('aircraft', 5, [('B', '2'), ('C', '2'), ('D', '2'), ('E', '2'), ('F', '2')]),
-             Boats('cruiser', 4, [('A', '4'), ('A', '5'), ('A', '6'), ('A', '7')]),
-             Boats('destroyer', 3, [('C', '5'), ('C', '6'), ('C', '7')]),
-             Boats('submarine', 3, [('H', '5'), ('I', '5'), ('J', '5')]),
-             Boats('torpedo', 2, [('E', '9'), ('F', '9')])
+             Boats('aircraft', [('B', '2'), ('C', '2'), ('D', '2'), ('E', '2'), ('F', '2')]),
+             Boats('cruiser', [('A', '4'), ('A', '5'), ('A', '6'), ('A', '7')]),
+             Boats('destroyer', [('C', '5'), ('C', '6'), ('C', '7')]),
+             Boats('submarine',  [('H', '5'), ('I', '5'), ('J', '5')]),
+             Boats('torpedo',  [('E', '9'), ('F', '9')])
          ]
 
     def play(self):
